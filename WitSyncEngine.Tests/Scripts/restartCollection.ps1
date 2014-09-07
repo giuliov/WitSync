30f2c44d4ce51620a8d949bc9e438717be833dd9
Giulio Vian <giuliovdev@gmail.com>
2014-09-07 18:12:38 +0200

    Integration test: script to manage snapshots of local DB

---
+++ WitSyncEngine.Tests/Scripts/restartCollection.ps1
@@ -0,0 +1,9 @@
+﻿. ./common.ps1
+
+# drop current and apply snapshot
+Invoke-Sqlcmd -Query "CREATE DATABASE [Tfs_WitSync] ON
+( FILENAME = N'$sqlDataPath\Tfs_WitSync.mdf' ),
+( FILENAME = N'$sqlDataPath\Tfs_WitSync_log.ldf' )
+FOR ATTACH" -ServerInstance .\SQLEXPRESS
+
+& $TfsConfig collection /attach /collectionDb:$env:COMPUTERNAME\SQLEXPRESS`;Tfs_WitSync /noprompt
