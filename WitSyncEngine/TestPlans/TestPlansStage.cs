05e375114bf824e79d9bec5fe956ca1908c2cad1
Giulio Vian <giuliovdev@gmail.com>
2015-01-23 17:43:02 +0100

    draft implementation
    Not working but gives a taste

--- WitSyncEngine/TestPlans/TestPlansStage.cs
+++ WitSyncEngine/TestPlans/TestPlansStage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.TeamFoundation.TestManagement.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,10 @@ namespace WitSync
             return 0;
         }
 
+        /*
+         * Some pointers http://blogs.msdn.com/b/densto/archive/2010/03/04/the-test-management-api-part-2-creating-modifying-test-plans.aspx
+         * http://blogs.msdn.com/b/duat_le/archive/2010/02/25/wiql-for-test.aspx
+         */
         public override int Execute(StageConfiguration configuration)
         {
             mapping = (TestPlansStageConfiguration)configuration;
@@ -39,19 +44,111 @@ namespace WitSync
             var destTestSvc = destConn.Collection.GetService<ITestManagementService>();
             var destProject = destTestSvc.GetTeamProject(destConn.ProjectName);
 
+            // sync contextual data
+            sourceProject.
+
+            var srcPlans = sourceProject.TestPlans.Query(mapping.SourceQuery);
+            foreach (ITestPlan2 srcPlan in srcPlans)
+            {
+                ITestPlan2 newPlan = (ITestPlan2)destProject.TestPlans.Create();
+                newPlan.AreaPath = mapAreaPath(srcPlan.AreaPath);
+                newPlan.AutomatedTestEnvironmentId = mapAutomatedTestEnvironment(srcPlan.AutomatedTestEnvironmentId);
+                newPlan.AutomatedTestSettingsId = mapAutomatedTestSettings(srcPlan.AutomatedTestSettingsId);
+                newPlan.CopyPropertiesFrom(srcPlan);
+                newPlan.Description = srcPlan.Description;
+                newPlan.EndDate = srcPlan.EndDate;
+                newPlan.Iteration = mapIteration(srcPlan.Iteration);
+                //newPlan.Links
+                newPlan.ManualTestEnvironmentId = mapManualTestEnvironment(srcPlan.ManualTestEnvironmentId);
+                newPlan.ManualTestSettingsId = mapManualTestSettings(srcPlan.ManualTestSettingsId);
+                newPlan.Name = srcPlan.Name;
+                newPlan.StartDate = srcPlan.StartDate;
+                newPlan.Status = srcPlan.Status;
+
+                var root = sourceProject.TestSuites.FetchTestSuitesForPlan(srcPlan, srcPlan.RootSuite.Id);
+                foreach (var srcSuite in root.SubSuites)
+                {
+                    ITestSuiteBase newSuite = null;
+                    switch (srcSuite.TestSuiteType)
+                    {
+                        case TestSuiteType.DynamicTestSuite:
+                            newSuite = destProject.TestSuites.CreateDynamic();
+                            break;
+                        case TestSuiteType.None:
+                            break;
+                        case TestSuiteType.RequirementTestSuite:
+                            WorkItem wi = null; //TODO lookup index
+                            newSuite = destProject.TestSuites.CreateRequirement(wi);
+                            break;
+                        case TestSuiteType.StaticTestSuite:
+                            newSuite = destProject.TestSuites.CreateStatic();
+                            //srcSuite.TestSuiteEntry
+                            break;
+                        default:
+                            break;
+                    }//switch
+                    newSuite.Description = srcSuite.Description;
+                    newSuite.Title = srcSuite.Title;
+
+                    var destCases = new List<ITestCase>();
+                    foreach (var srcCase in srcSuite.AllTestCases)
+                    {
+                        // Assume it was cloned in Work Item stage
+                        int it = mapTestCase(srcCase.Id);
+                        ITestCase destCase = destProject.TestCases.Find(it);
+
+                        // destCase.TestSuiteEntry ???
+
+                        destCases.Add(destCase);
+                    }
+                    newSuite.TestCases.AddCases(destCases, true);
+                }
+            }
 
             if (!configuration.TestOnly)
             {
                 /*
-                var newPlan = destProject.TestPlans.Create();
-                var newSuite = destProject.TestSuites.CreateStatic();
-                newSuite.TestCases.AddCases();
                 newPlan.Save();
-                 * */
                 this.ChangeLog.AddEntry(new TestPlanChangeEntry("TODO"));
+                 * */
             }
 
             return saveErrors;
+        }
+
+        private int mapTestCase(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string mapIteration(string iterationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Guid mapManualTestEnvironment(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int mapManualTestSettings(int settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int mapAutomatedTestSettings(int settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Guid mapAutomatedTestEnvironment(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string mapAreaPath(string areaPath)
+        {
+            throw new NotImplementedException();
         }
     }
 }
